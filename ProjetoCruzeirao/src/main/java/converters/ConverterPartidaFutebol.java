6389671905e548dde1cd93484a91e43c3d525aee
senwa <senwa@DESKTOP-S53H5EK>
2018-04-05 03:29:54 -0300

    - Outros.

---
+++ ProjetoCruzeirao/src/main/java/converters/ConverterPartidaFutebol.java
@@ -0,0 +1,39 @@
+package converters;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+import entidades.PartidasFutebol;
+
+
+@FacesConverter(value = "ConverterPartidaFutebol")
+public class ConverterPartidaFutebol implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String value) {
+			
+		if(value != null && !value.isEmpty()) 
+				return (PartidasFutebol) uiComponent.getAttributes().get(value);
+
+			return null;
+	}
+
+	@Override
+	public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
+		
+		if(value instanceof PartidasFutebol) {			
+			PartidasFutebol partidafutebol = (PartidasFutebol) value;
+			
+			if(partidafutebol != null && partidafutebol instanceof PartidasFutebol && Integer.toString((int) partidafutebol.getNumero()) != null) {
+				
+				uiComponent.getAttributes().put(Integer.toString((int) partidafutebol.getNumero()), partidafutebol);
+				return Integer.toString((int) partidafutebol.getNumero());
+			}	
+			
+		}
+		
+		return "";
+	}
+
+}
